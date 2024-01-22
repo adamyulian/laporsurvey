f5718796cdb59f6aba86bbbb37af1f4c6c43d35e
aadamyulian <aadamyulian@gmail.com>
2024-01-22 17:04:15 +0700

    cek

--- app/Filament/Resources/SurveyResource.php
+++ app/Filament/Resources/SurveyResource.php
@@ -495,7 +495,7 @@ class SurveyResource extends Resource
                         TextEntry::make('lng'),
                         TextEntry::make('address')
                                 ->label('Location Data Saved'),
-                        TextEntry::make('surveyor')
+                        TextEntry::make('surveyor.nama')
                                 ->label('Surveyor')
                     ])
                 ])->columnSpan(1)
