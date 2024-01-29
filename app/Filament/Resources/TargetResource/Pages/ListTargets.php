9aee63978ece4b89b05a37685e23fdcf9e7a7306
aadamyulian <aadamyulian@gmail.com>
2024-01-29 09:58:33 +0700

    filter selesai / belum

--- app/Filament/Resources/TargetResource/Pages/ListTargets.php
+++ app/Filament/Resources/TargetResource/Pages/ListTargets.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\TargetResource\Pages;
 
-use App\Filament\Resources\TargetResource;
 use Filament\Actions;
+use Illuminate\Support\Facades\Auth;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\TargetResource;
+use Filament\Resources\Pages\ListRecords\Tab;
 
 class ListTargets extends ListRecords
 {
@@ -14,6 +17,42 @@ class ListTargets extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'All' => Tab::make('All Target')
+                ->modifyQueryUsing(function (Builder $query) {
+                    if (Auth::user()->role === 'admin') {
+                        return $query;
+                    }
+                    // Non-admin users can only view their own component
+                    // return 
+                        $teamname = Auth::user()->team->name;
+                        $query->where('surveyor', $teamname);
+                    }),
+            'Belum' => Tab::make('Belum')
+                ->modifyQueryUsing(function (Builder $query) {
+                    if (Auth::user()->role === 'admin') {
+                        return $query;
+                    }
+                    // Non-admin users can only view their own component
+                    // return 
+                        $teamname = Auth::user()->team->name;
+                        $query->where('surveyor', $teamname)->where('user_id', 0);
+                    }),
+            'Selesai' => Tab::make('Selesai')
+                ->modifyQueryUsing(function (Builder $query) {
+                if (Auth::user()->role === 'admin') {
+                    return $query;
+                }
+                // Non-admin users can only view their own component
+                // return 
+                    $teamname = Auth::user()->team->name;
+                    $query->where('surveyor', $teamname)->where('user_id','!=', 0);
+                }),
         ];
     }
 }
