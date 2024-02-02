f2a844b38ba37d5bf2ff2f8df273cbfd4ef8ca24
aadamyulian <aadamyulian@gmail.com>
2024-02-02 19:25:45 +0700

    l

--- app/Filament/Resources/SurveykendaraanResource/Pages/ListSurveykendaraans.php
+++ app/Filament/Resources/SurveykendaraanResource/Pages/ListSurveykendaraans.php
@@ -13,7 +13,8 @@ class ListSurveykendaraans extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->label('New Survey'),
         ];
     }
 }
