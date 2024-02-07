ef01811880f087ffe4a64da3060cb0227ae29208
aadamyulian <aadamyulian@gmail.com>
2024-02-07 11:28:33 +0700

    w

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -4,6 +4,7 @@ namespace App\Filament\Widgets;
 
 use App\Models\Survey;
 use App\Models\Target;
+use Filament\Infolists\Components\Section;
 use Illuminate\Support\Facades\Auth;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
@@ -23,13 +24,17 @@ class StatsOverview extends BaseWidget
         if ($user && ($user->role === 'admin')) {
             // Admin gets all data
             return [
-                Stat::make('Total Target Aset Tanah', Target::count()),
-                Stat::make('Total Survey Aset Tanah', Survey::count()),
-                Stat::make('Sisa Target Survey', Target::where('user_id',0)->count()),
-                Stat::make('Total Survey Group 1', Survey::where('team_id', 1)->count()),
-                Stat::make('Total Survey Group 2', Survey::where('team_id', 2)->count()),
-                Stat::make('Total Survey Group 3', Survey::where('team_id', 3)->count()),
-                Stat::make('Total Survey Group 4', Survey::where('team_id', 4)->count()),
+                Section::make('Survey Aset Tanah')
+                ->schema([
+                    Stat::make('Total Target Aset Tanah', Target::count()),
+                    Stat::make('Total Survey Aset Tanah', Survey::count()),
+                    Stat::make('Sisa Target Survey', Target::where('user_id',0)->count()),
+                    Stat::make('Total Survey Group 1', Survey::where('team_id', 1)->count()),
+                    Stat::make('Total Survey Group 2', Survey::where('team_id', 2)->count()),
+                    Stat::make('Total Survey Group 3', Survey::where('team_id', 3)->count()),
+                    Stat::make('Total Survey Group 4', Survey::where('team_id', 4)->count()),
+                ])
+                
                
             ];
         } elseif ($user->role === 'ketua') {
