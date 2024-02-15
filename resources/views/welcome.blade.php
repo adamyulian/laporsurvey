59b8d2dabcae1810ff5b179b367bdc2f2282c64b
aadamyulian <aadamyulian@gmail.com>
2024-02-15 21:58:29 +0700

    sasasa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,21 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selamat Datang di Proyekan</title>
+    <!-- Include Tailwind CSS via CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100 flex items-center justify-center h-screen">
+    <div class="text-center">
+        <h1 class="text-4xl font-bold mb-4">Selamat Datang di Halaman Pengecekan Aset per Wilayah <br> Pemerintah Kota Surabaya</h1>
+        <a href="/admin/login" class="bg-yellow-500 text-white font-bold py-2 px-4 rounded-full hover:bg-yellow-700">MASUK DISINI</a>
+    </div>
+</body>
+</html>
+
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -137,4 +154,4 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
