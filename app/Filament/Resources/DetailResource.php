a042a5651a72f1a812769d2ebc4c94eba503dec8
aadamyulian <aadamyulian@gmail.com>
2024-02-18 10:18:40 +0700

    xxx

--- app/Filament/Resources/DetailResource.php
+++ app/Filament/Resources/DetailResource.php
@@ -112,7 +112,7 @@ class DetailResource extends Resource
             ])
             ->actions([
                 // Tables\Actions\ViewAction::make(),
-                // Tables\Actions\EditAction::make(),
+                Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
