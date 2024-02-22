60e079c87bfe1c9ac8e4af655136b51371b0f8d6
aadamyulian <aadamyulian@gmail.com>
2024-02-22 11:23:23 +0700

    xxxxxxxxxxxx

--- app/Filament/Resources/SurveyResource/Pages/ListSurveys.php
+++ app/Filament/Resources/SurveyResource/Pages/ListSurveys.php
@@ -36,7 +36,7 @@ class ListSurveys extends ListRecords
             'Tanah' => Tab::make('Semua Tanah')
                 ->modifyQueryUsing(function (Builder $query) {
                     if (Auth::user()->role === 'admin') {
-                        return $query;
+                        return $query->where('target.kode_barang', 'LIKE', '%1.3.1.%');
                     }
                     // Non-admin users can only view their own component
                     // return 
